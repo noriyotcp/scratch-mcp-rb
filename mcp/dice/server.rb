521fa8c3eb7f8227ef80f94c75c69ecde97afaea
noriyotcp <noriyo.akita@gmail.com>
2025-05-06 09:54:23 +0900

    WIP: process_message

--- mcp/dice/server.rb
+++ mcp/dice/server.rb
@@ -1,27 +1,47 @@
 require 'json'
 
-print JSON.generate({
-  jsonrpc: '2.0',
-  id: 1,
-  result: {
-    protocolVersion: '2024-11-05',
-    capabilities: {
-      logging: {},
-      prompts: {
-        listChanged: true
-      },
-      resources: {
-        subscribe: true,
-        listChanged: true
-      },
-      tools: {
-        listChanged: true
-      }
-    },
-    serverInfo: {
-      name: 'MCP Client',
-      version: '1.0.0'
-    },
-    instructions: 'Optional instructions for the client'
-  }
-})
+module MCP
+  module Dice
+    class Server
+      def initialize(stdin, stdout)
+        @stdin = stdin
+        @stdout = stdout
+      end
+
+      def process_message(message)
+        request = JSON.parse(message, symbolize_names: true)
+
+        case request[:method]
+        when 'initialize'
+          JSON.generate({
+            jsonrpc: '2.0',
+            id: 1,
+            result: {
+              protocolVersion: '2024-11-05',
+              capabilities: {
+                logging: {},
+                prompts: {
+                  listChanged: true
+                },
+                resources: {
+                  subscribe: true,
+                  listChanged: true
+                },
+                tools: {
+                  listChanged: true
+                }
+              },
+              serverInfo: {
+                name: 'MCP Client',
+                version: '1.0.0'
+              },
+              instructions: 'Optional instructions for the client'
+            }
+          })
+        when 'notifications/initialized'
+          nil
+        end
+      end
+    end
+  end
+end
